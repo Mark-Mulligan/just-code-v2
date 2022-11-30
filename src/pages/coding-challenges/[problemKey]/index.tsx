89f1ae3b3378ce838ad09afb43a53e21f4172cfa
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-11-30 16:19:11 -0600

    improved styling on code challenge page

--- src/pages/coding-challenges/[problemKey]/index.tsx
+++ src/pages/coding-challenges/[problemKey]/index.tsx
@@ -4,9 +4,14 @@ import { useState } from "react";
 // Next
 import { NextPage } from "next";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
 
 // Supabase
 import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { Session, User } from "@supabase/auth-helpers-nextjs";
+
+// uuid
+import { v4 as uuidv4 } from "uuid";
 
 // Resizable
 import { Resizable } from "re-resizable";
@@ -15,8 +20,22 @@ import { Resizable } from "re-resizable";
 import CodeMirror from "@uiw/react-codemirror";
 import { javascript } from "@codemirror/lang-javascript";
 
-const CodingExercise: NextPage = () => {
-  const [userCode, setUserCode] = useState("");
+// Data
+import { codingChallengesData } from "../../../../data/codingChallengeData";
+
+// Custom Types
+import { IParams, CodingChallengeData } from "../../../../types/customTypes";
+
+interface IProps {
+  codingChallengeData: CodingChallengeData;
+  initialSession: Session;
+  user: User;
+}
+
+const CodingExercise: NextPage<IProps> = ({ codingChallengeData }) => {
+  const [userCode, setUserCode] = useState(
+    codingChallengeData?.startingCode || ""
+  );
 
   return (
     <div className="flex h-screen">
@@ -34,12 +53,25 @@ const CodingExercise: NextPage = () => {
         }}
         defaultSize={{ width: 500, height: "auto" }}
       >
-        <h1 className="mb-2 text-2xl font-bold">Sum Two Ints</h1>
-        <p className="font-light">
-          Create a function that takes in two arguments (num1 & num2 that will
-          both be integers) and returns a number that is the sum of the two
-          integers provided.
-        </p>
+        <h1 className="mb-2 text-2xl font-bold">{codingChallengeData.title}</h1>
+        <p className="mb-3 font-light">{codingChallengeData.instructions}</p>
+        <h4 className="mb-2 text-xl font-bold">Test Criteria</h4>
+        <ul className="mb-4">
+          {codingChallengeData.testCriteria.map((item) => {
+            return (
+              <li className="font-light" key={uuidv4()}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+        <h4 className="mb-2 text-xl font-bold">Test Results</h4>
+        <p className="ton-light mb-4">Submit code to get test results.</p>
+        <button className="btn-primary btn-block btn mb-3">Submit Code</button>
+        <button className="btn-block btn mb-3">Help</button>
+        <Link href="/coding-challenges" className="btn-block btn">
+          Back To Challenges
+        </Link>
       </Resizable>
       <div className="w-full pt-[4rem]" style={{ background: "#282c34" }}>
         <CodeMirror
@@ -64,6 +96,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     data: { session },
   } = await supabase.auth.getSession();
 
+  const { problemKey } = ctx.params as IParams;
+  const codingChallengeData = codingChallengesData[problemKey];
+
   if (!session)
     return {
       redirect: {
@@ -76,6 +111,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
     props: {
       initialSession: session,
       user: session.user,
+      codingChallengeData,
     },
   };
 };
