bb67ec1d125f72eb6736aa24066f74bb56b6b409
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-12-29 20:13:06 -0600

    updated tests

--- data/utils.ts
+++ data/utils.ts
@@ -1,25 +1,23 @@
 import { TestResult } from '../types/customTypes';
 
 interface testUtilFunction {
-    name: string;
-    func: Function;
+  name: string;
+  func: Function;
 }
 
 export const createTestScriptString = (
-    testFunc: Function,
-    utilFuncs?: testUtilFunction[]
+  testFunc: Function,
+  utilFuncs?: testUtilFunction[]
 ) => {
-    let resultStr = '';
-    if (utilFuncs && utilFuncs.length > 0) {
-        utilFuncs.forEach((utilFunc) => {
-            resultStr += `const ${
-                utilFunc.name
-            } = ${utilFunc.func.toString()} \n`;
-        });
-    }
+  let resultStr = '';
+  if (utilFuncs && utilFuncs.length > 0) {
+    utilFuncs.forEach((utilFunc) => {
+      resultStr += `const ${utilFunc.name} = ${utilFunc.func.toString()} \n`;
+    });
+  }
 
-    resultStr += `const runTests = ${testFunc.toString()} \n runTests();`;
-    return resultStr;
+  resultStr += `const runTests = ${testFunc.toString()} \n runTests();`;
+  return resultStr;
 };
 
 const isEqualFunctionAsString = `function areEqual(arg1, arg2) {
@@ -61,140 +59,144 @@ const isEqualFunctionAsString = `function areEqual(arg1, arg2) {
 type ReturnTypes = 'string' | 'number' | 'array' | 'object';
 
 export const generateTestScriptString = (
-    funcName: string,
-    returnType: ReturnTypes,
-    tests: { input?: any[]; result: any }[]
+  funcName: string,
+  returnType: ReturnTypes,
+  tests: { input?: any[]; result: any }[]
 ) => {
-    let testScriptCode = '';
-    const testCriteria: string[] = [];
-    let testsAsString = '';
+  let testScriptCode = '';
+  const testCriteria: string[] = [];
+  let testsAsString = '';
 
-    tests.forEach((test, index) => {
-        let argsAsString;
-        if (test.input) {
-            argsAsString = test.input
-                .map((item) => JSON.stringify(item))
-                .join(',');
-        }
+  tests.forEach((test, index) => {
+    let argsAsString;
+    if (test.input) {
+      argsAsString = test.input.map((item) => JSON.stringify(item)).join(',');
+    }
 
-        const functionCallString = `${funcName}(${argsAsString || ''})`;
-        const resultAsString = JSON.stringify(test.result);
+    const functionCallString = `${funcName}(${argsAsString || ''})`;
+    const resultAsString = JSON.stringify(test.result);
 
-        if (index === 0) {
-            let testDescription = `User created a function called ${funcName}`;
-            let passed = `typeof ${funcName} === "function"`;
-            let result = `typeof ${funcName}`;
-            testsAsString += `testResults.push({ test: '${testDescription}', passed: ${passed}, result: ${result}})\n`;
-            testCriteria.push(testDescription);
+    if (index === 0) {
+      let testDescription = `User created a function called ${funcName}`;
+      let passed = `typeof ${funcName} === "function"`;
+      let result = `typeof ${funcName}`;
+      testsAsString += `testResults.push({ test: '${testDescription}', passed: ${passed}, result: ${result}})\n`;
+      testCriteria.push(testDescription);
 
-            if (returnType === 'array') {
-                passed = `Array.isArray(${functionCallString})`;
-            } else {
-                passed = `typeof ${functionCallString} === "${returnType}"`;
-            }
-            testDescription = `${functionCallString} returns a ${returnType}`;
-            result = `typeof ${functionCallString}`;
-            testsAsString += `testResults.push({ test: '${testDescription}', passed: ${passed}, result: ${result}})\n`;
-            testCriteria.push(testDescription);
-        }
+      if (returnType === 'array') {
+        passed = `Array.isArray(${functionCallString})`;
+      } else {
+        passed = `typeof ${functionCallString} === "${returnType}"`;
+      }
 
-        const testDescription = `${functionCallString} returns ${resultAsString}`;
-        const passed = `areEqual(${functionCallString}, ${resultAsString})`;
-        const result = `JSON.stringify(${functionCallString})`;
-        const testResult = `{ test: '${testDescription}', passed: ${passed}, result: ${result}}`;
-        testsAsString += `testResults.push(${testResult})\n`;
-        testCriteria.push(testDescription);
-    });
+      if (returnType === 'array' || returnType === 'object') {
+        testDescription = `${functionCallString} returns an ${returnType}`;
+      } else {
+        testDescription = `${functionCallString} returns a ${returnType}`;
+      }
 
-    testScriptCode += `${isEqualFunctionAsString}\n
+      result = `typeof ${functionCallString}`;
+      testsAsString += `testResults.push({ test: '${testDescription}', passed: ${passed}, result: ${result}})\n`;
+      testCriteria.push(testDescription);
+    }
+
+    const testDescription = `${functionCallString} returns ${resultAsString}`;
+    const passed = `areEqual(${functionCallString}, ${resultAsString})`;
+    const result = `JSON.stringify(${functionCallString})`;
+    const testResult = `{ test: '${testDescription}', passed: ${passed}, result: ${result}}`;
+    testsAsString += `testResults.push(${testResult})\n`;
+    testCriteria.push(testDescription);
+  });
+
+  testScriptCode += `${isEqualFunctionAsString}\n
   const runTests = () => {
     const testResults = [];
     ${testsAsString}
     return testResults;
   }\n
   runTests()`;
-    return { testScriptCode, testCriteria };
+  return { testScriptCode, testCriteria };
 };
 
 export const extractTestCriteria = (testLogic: TestResult[]) => {
-    return testLogic.map((testResult) => {
-        return testResult.test;
-    });
+  return testLogic.map((testResult) => {
+    return testResult.test;
+  });
 };
 
 export const deepEqual = (
-    obj1: { [key: string]: any },
-    obj2: { [key: string]: any }
+  obj1: { [key: string]: any },
+  obj2: { [key: string]: any }
 ) => {
-    if (obj1 === obj2) return true;
+  if (obj1 === obj2) return true;
 
-    if (isPrimitive(obj1) && isPrimitive(obj2)) return obj1 === obj2;
+  if (isPrimitive(obj1) && isPrimitive(obj2)) return obj1 === obj2;
 
-    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
 
-    for (let key in obj1) {
-        if (!(key in obj2)) return false;
-        if (!deepEqual(obj1[key], obj2[key])) return false;
-    }
+  for (let key in obj1) {
+    if (!(key in obj2)) return false;
+    if (!deepEqual(obj1[key], obj2[key])) return false;
+  }
 
-    return true;
+  return true;
 };
 
 function isPrimitive(obj: { [key: string]: any }) {
-    return obj !== Object(obj);
+  return obj !== Object(obj);
 }
 
 export function object_equals(x: any, y: any) {
-    if (x === y) return true;
-    if (!(x instanceof Object) || !(y instanceof Object)) return false;
-    if (x.constructor !== y.constructor) return false;
+  if (x === y) return true;
+  if (!(x instanceof Object) || !(y instanceof Object)) return false;
+  if (x.constructor !== y.constructor) return false;
 
-    for (var p in x) {
-        if (!x.hasOwnProperty(p)) continue;
-        if (!y.hasOwnProperty(p)) return false;
-        if (x[p] === y[p]) continue;
-        if (typeof x[p] !== 'object') return false;
-        if (!object_equals(x[p], y[p])) return false;
-    }
+  for (var p in x) {
+    if (!x.hasOwnProperty(p)) continue;
+    if (!y.hasOwnProperty(p)) return false;
+    if (x[p] === y[p]) continue;
+    if (typeof x[p] !== 'object') return false;
+    if (!object_equals(x[p], y[p])) return false;
+  }
 
-    for (p in y) if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) return false;
+  for (p in y) if (y.hasOwnProperty(p) && !x.hasOwnProperty(p)) return false;
 
-    return true;
+  return true;
 }
 
 export const largestSumReturnTypeCheck = (largestSumResult: {
-    largestSum: number;
-    indexes: number[];
+  largestSum: number;
+  indexes: number[];
 }) => {
-    if (typeof largestSumResult !== 'object') {
-        return false;
-    }
+  if (typeof largestSumResult !== 'object') {
+    return false;
+  }
 
-    if (Object.keys(largestSumResult).length !== 2) {
-        return false;
-    }
+  if (Object.keys(largestSumResult).length !== 2) {
+    return false;
+  }
 
-    if (
-        !largestSumResult.hasOwnProperty('largestSum') ||
-        !largestSumResult.hasOwnProperty('indexes')
-    ) {
-        return false;
-    }
+  if (
+    !largestSumResult.hasOwnProperty('largestSum') ||
+    !largestSumResult.hasOwnProperty('indexes')
+  ) {
+    return false;
+  }
 
-    if (
-        typeof largestSumResult['largestSum'] !== 'number' ||
-        !Array.isArray(largestSumResult['indexes']) ||
-        largestSumResult['indexes'].length !== 2
-    ) {
-        return false;
-    }
+  if (
+    typeof largestSumResult['largestSum'] !== 'number' ||
+    !Array.isArray(largestSumResult['indexes']) ||
+    largestSumResult['indexes'].length !== 2
+  ) {
+    return false;
+  }
 
-    if (
-        typeof largestSumResult['indexes'][0] !== 'number' ||
-        typeof largestSumResult['indexes'][1] !== 'number'
-    ) {
-        return false;
-    }
+  if (
+    typeof largestSumResult['indexes'][0] !== 'number' ||
+    typeof largestSumResult['indexes'][1] !== 'number'
+  ) {
+    return false;
+  }
 
-    return true;
+  return true;
 };
