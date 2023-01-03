1788ee31df7d8229cf66d94a496041684cf617b9
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2023-01-02 20:38:28 -0600

    refactored more challenges tests

--- data/challenges/calculateTheTip.ts
+++ data/challenges/calculateTheTip.ts
@@ -1,8 +1,8 @@
 // Testing Utils
-import { createTestScriptString, extractTestCriteria } from "../utils";
+import { generateTestScriptString } from '../utils';
 
 // Custom Types
-import { TestResult, CodingChallengeData } from "../../types/customTypes";
+import { CodingChallengeData } from '../../types/customTypes';
 
 const calculateTheTipSolution = `const tipCalc = (mealPrice, tipPercent) => {
   let mealPriceAsNum = Number(mealPrice.slice(1));
@@ -12,74 +12,37 @@ const calculateTheTipSolution = `const tipCalc = (mealPrice, tipPercent) => {
   return \`$\${tipCostAsNum.toFixed(2)}\`;
 }`;
 
-const tipCalc = (bill: string, pctTip: string) => {
-  return "";
-};
-
-const calculateTheTipTests = () => {
-  const testResults: TestResult[] = [];
-  testResults.push({
-    test: "User created a function tipCalc.",
-    passed: typeof tipCalc === "function",
-    result: typeof tipCalc,
-  });
-  testResults.push({
-    test: "Function returns a string.",
-    passed: typeof tipCalc("$25.97", "20%") === "string",
-    result: typeof tipCalc("$25.97", "20%"),
-  });
-  testResults.push({
-    test: 'tipCalc("$25.97", "20%") returns $5.19',
-    passed: tipCalc("$25.97", "20%") === "$5.19",
-    result: tipCalc("$25.97", "20%"),
-  });
-  testResults.push({
-    test: 'tipCalc("$43.28", "21%") returns $9.09',
-    passed: tipCalc("$43.28", "21%") === "$9.09",
-    result: tipCalc("$43.28", "21%"),
-  });
-  testResults.push({
-    test: 'tipCalc("$12.63", "18%") returns $2.27',
-    passed: tipCalc("$12.63", "18%") === "$2.27",
-    result: tipCalc("$12.63", "18%"),
-  });
-  testResults.push({
-    test: 'tipCalc("101.73", "25%") returns $25.43',
-    passed: tipCalc("$101.73", "25%") === "$25.43",
-    result: tipCalc("$101.73", "25%"),
-  });
-  testResults.push({
-    test: 'tipCalc("$10.91", "5%") returns $0.55',
-    passed: tipCalc("$10.91", "5%") === "$0.55",
-    result: tipCalc("$10.91", "5%"),
-  });
-  testResults.push({
-    test: 'tipCalc("$32.12, "19%") returns $6.10',
-    passed: tipCalc("$32.12", "19%") === "$6.10",
-    result: tipCalc("$32.12", "19%"),
-  });
-
-  return testResults;
-};
+const { testCriteria, testScriptCode } = generateTestScriptString(
+  'tipCalc',
+  'string',
+  [
+    { input: ['$25.97', '20%'], result: '$5.19' },
+    { input: ['$43.28', '21%'], result: '$9.09' },
+    { input: ['$12.63', '18%'], result: '$2.27' },
+    { input: ['$101.73', '25%'], result: '$25.43' },
+    { input: ['$10.91', '5%'], result: '$0.55' },
+    { input: ['$32.12', '19%'], result: '$6.10' },
+  ]
+);
 
 const data: CodingChallengeData = {
-  title: "Tip Calculator",
+  title: 'Tip Calculator',
   description:
-    "Create function that determines how much tip should be left on a meal dining out.",
+    'Create function that determines how much tip should be left on a meal dining out.',
   instructions:
     'Create a function that takes in two arguments, the meal price (a string, ex. "$24.94") and the tip percent (a string, ex. "20%"), and returns the amount the tip should be. This amount should be return as a string in dollar format (ex. "$5.23", $10.00"). Include two digits for the decimal place even if they are 0.',
   difficulty: 2,
-  testScriptCode: createTestScriptString(calculateTheTipTests),
+  testScriptCode,
   startingCode:
-    "const tipCalc = (mealPrice, tipPercent) => {\n  // Add Code Below\n\n\n  // Add Code Above\n}",
-  testCriteria: extractTestCriteria(calculateTheTipTests()),
+    'const tipCalc = (mealPrice, tipPercent) => {\n  // Add Code Below\n\n\n  // Add Code Above\n}',
+  testCriteria,
   problemExplanation:
-    "This problem will require you to convert strings to numbers and back to a string. It also deals with some import aspects in programming such as dealing with percents and money values.",
+    'This problem will require you to convert strings to numbers and back to a string. It also deals with some import aspects in programming such as dealing with percents and money values.',
   hints: [
-    "You will need to take the meal price and convert it to a number",
-    "You will need to convert the tip percent to a floating point number (decimal)",
-    "You will need to round the result to the nearest hundredth",
-    "The result needs to be converted to money format (starts with a $ and always has two decimal numbers even if they are 0)",
+    'You will need to take the meal price and convert it to a number',
+    'You will need to convert the tip percent to a floating point number (decimal)',
+    'You will need to round the result to the nearest hundredth',
+    'The result needs to be converted to money format (starts with a $ and always has two decimal numbers even if they are 0)',
   ],
   solutionCode: calculateTheTipSolution,
 };
--- data/challenges/historicalDay.ts
+++ data/challenges/historicalDay.ts
@@ -1,8 +1,8 @@
 // Testing Utils
-import { createTestScriptString, extractTestCriteria } from "../utils";
+import { generateTestScriptString } from '../utils';
 
 // Custom Types
-import { TestResult, CodingChallengeData } from "../../types/customTypes";
+import { CodingChallengeData } from '../../types/customTypes';
 
 const historicalDaySolution = `const findDay = (dateStr) => {
   let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -12,75 +12,34 @@ const historicalDaySolution = `const findDay = (dateStr) => {
   return weekDays[dayNum];
 }`;
 
-const findDay = (dateStr: string) => {
-  let weekDays = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let givenDate = new Date(dateStr);
-  let dayNum = givenDate.getDay();
-
-  return weekDays[dayNum];
-};
-
-const historicalDayTests = () => {
-  const testResults: TestResult[] = [];
-  testResults.push({
-    test: "User created a function findDay.",
-    passed: typeof findDay === "function",
-    result: typeof findDay,
-  });
-  testResults.push({
-    test: "Function returns a string.",
-    passed: typeof findDay("10-19-1987") === "string",
-    result: typeof findDay("10-19-1987"),
-  });
-  testResults.push({
-    test: 'findDay("10-19-1987") returns "Monday"',
-    passed: findDay("10-19-1987") === "Monday",
-    result: findDay("10-19-1987"),
-  });
-  testResults.push({
-    test: 'findDay("05-25-1977") returns "Wednesday"',
-    passed: findDay("05-25-1977") === "Wednesday",
-    result: findDay("05-25-1977"),
-  });
-  testResults.push({
-    test: 'findDay("07-21-1969") returns "Monday"',
-    passed: findDay("07-21-1969") === "Monday",
-    result: findDay("07-21-1969"),
-  });
-  testResults.push({
-    test: 'findDay("06-29-2007") returns "Friday"',
-    passed: findDay("06-29-2007") === "Friday",
-    result: findDay("06-29-2007"),
-  });
-
-  return testResults;
-};
+const { testCriteria, testScriptCode } = generateTestScriptString(
+  'findDay',
+  'string',
+  [
+    { input: ['10-19-1987'], result: 'Monday' },
+    { input: ['05-25-1977'], result: 'Wednesday' },
+    { input: ['07-21-1969'], result: 'Monday' },
+    { input: ['06-29-2007'], result: 'Friday' },
+  ]
+);
 
 const data: CodingChallengeData = {
-  title: "Historical Day",
+  title: 'Historical Day',
   description:
-    "Create a function that takes a date string and returns what day of the week that date occurred on.",
+    'Create a function that takes a date string and returns what day of the week that date occurred on.',
   instructions:
     'Create a function that takes in one argument, a date string formatted like "10-19-1987", and returns what day of the week that date occurred on.  Return values should be one of the follow: Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, or Sunday.',
   difficulty: 2,
-  testScriptCode: createTestScriptString(historicalDayTests),
+  testScriptCode,
   startingCode:
-    "const findDay = (dateStr) => {\n  // Add Code Below\n\n\n  // Add Code Above\n}",
-  testCriteria: extractTestCriteria(historicalDayTests()),
+    'const findDay = (dateStr) => {\n  // Add Code Below\n\n\n  // Add Code Above\n}',
+  testCriteria,
   problemExplanation:
-    "This problem will require you to do a couple of different things. One, you will need to convert the date string to a date object.  Then use that date object to return a string matching the desired results.",
+    'This problem will require you to do a couple of different things. One, you will need to convert the date string to a date object.  Then use that date object to return a string matching the desired results.',
   hints: [
-    "You can create a date object by using new Date(pass dateStr)",
-    "dateObj.getDay() returns the day of the week for a given date as a number (0 is Sunday, 6 is Saturday)",
-    "Maybe store the day of the week results as strings in an data structure that can be indexed.",
+    'You can create a date object by using new Date(pass dateStr)',
+    'dateObj.getDay() returns the day of the week for a given date as a number (0 is Sunday, 6 is Saturday)',
+    'Maybe store the day of the week results as strings in an data structure that can be indexed.',
   ],
   solutionCode: historicalDaySolution,
 };
