4293843b207e489b2f18fc4bf0eac162faab8105
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2023-01-03 21:23:28 -0600

    allowing site to work locally and deployed for sign in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,18 @@ import { BsGithub, BsGoogle } from 'react-icons/bs';
 // Components
 import LoadingScreen from '../components/LoadingScreen';
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    'http://localhost:3000/';
+  // Make sure to include `https://` when not localhost.
+  url = url.includes('http') ? url : `https://${url}`;
+  // Make sure to including trailing `/`.
+  url = url.charAt(url.length - 1) === '/' ? url : `${url}/`;
+  return url;
+};
+
 const Home: NextPage = () => {
   const session = useSession();
   const supabase = useSupabaseClient();
@@ -24,12 +36,18 @@ const Home: NextPage = () => {
   const signInWithGithub = async () => {
     await supabase.auth.signInWithOAuth({
       provider: 'github',
+      options: {
+        redirectTo: getURL(),
+      },
     });
   };
 
   const signInWithGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
+      options: {
+        redirectTo: getURL(),
+      },
     });
   };
 
